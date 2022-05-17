0315da4354f2dfc5845dea406caf3068236a650c
Önder ERTAN <nigbuk1998@hotmail.com>
2022-05-17 16:52:33 +0300

    Update

--- img-to-b64.py
+++ img-to-b64.py
@@ -1,12 +1,39 @@
 import base64
+import os
 from PIL import Image
-import pyperclip #install this module for further use (console>pip install pyperclip)
+# install this module for further use (console>pip install pyperclip)
+import pyperclip 
 
-sizex=sizey=150 #image width height
-image = Image.open('targetimage.jpeg')
-resizedimage=image.resize((sizex,sizey))#resizing image
-resizedimage.save('resizedimage.jpeg') #saves resized image
+# ---------------------------------SECTION I--------------------------------- #
 
-with open("resizedimage.jpeg","rb") as img_file: #open image file
-    base64_string = base64.b64encode(img_file.read()) #encoding image to base64 string
-pyperclip.copy(base64_string.decode('utf-8'))  #decodes from bytes to string and copies to clipboard
+# get the image from the user
+path=input("Paste the complete path of the image:")
+image = Image.open(path)
+
+# ---------------------------------SECTION II-------------------------------- #
+
+# get image width&height in pixel, image is considered to be in 1:1 ratio
+sizex=sizey=int(input("Type the size for the image (Enter for default 150px):"))  
+if not sizex and not sizey:
+    # if size is empty pass 150 as default sizes into the resize function
+    resizedimage=image.resize((150,150))
+else:
+    # if size is not empty, pass the value to the resize function
+    resizedimage=image.resize((sizex,sizey))
+    
+# --------------------------------SECTION III-------------------------------- #
+
+# save resized image
+resizedimage.save(os.path.basename(path)+'resized.jpeg') 
+
+# ---------------------------------SECTION IV-------------------------------- #
+
+# open image file
+with open(os.path.basename(path)+"resized.jpeg","rb") as img_file:
+    #   encode image to base64 string
+    base64_string = base64.b64encode(img_file.read())
+    
+# --------------------------------SECTION V---------------------------------- #
+
+# decode from bytes to string and copy to clipboard
+pyperclip.copy(base64_string.decode('utf-8'))  
